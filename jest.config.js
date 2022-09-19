4995c7264384374a9dc70e32eb6c47782e68f4c6
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2022-09-18 18:52:27 -0700

    chore: 🔧 upgrade packages + add import order sorting via prettier

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   coveragePathIgnorePatterns: ['/node_modules/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-      diagnostics: false,
-    },
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        diagnostics: false,
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
