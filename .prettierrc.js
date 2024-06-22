780dff3b02ccb6c4d686ad9a345064619144a501
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2024-06-21 22:30:36 -0700

    chore: 🔧 upgrade packages

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  singleQuote: true,
+  noSemi: true,
+  semi: false,
+  trailingComma: "all",
+  printWidth: 120,
+  endOfLine: "auto",
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^[../]",
+    "^[./]",
+  ],
+};
+
+module.exports = config;
